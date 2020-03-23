bb6d9744c6e23ac3f1bb2c442d6bb3eba4a860ce
Piotr Damer <piotrd@czuby.net>
2020-03-23 22:40:28 +0000

    Fixed the stale timestamps from source .csv files

--- covid-19_plots.py
+++ covid-19_plots.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[90]:
+# In[224]:
 
 
 import pandas as pd
@@ -9,9 +9,11 @@ from datetime import datetime, timedelta
 from os import path
 import glob
 import matplotlib.pyplot as plt
+import numpy as np
+import re
 
 
-# In[62]:
+# In[225]:
 
 
 date = datetime.today() - timedelta(days = 1)
@@ -32,24 +34,35 @@ while date >= min_date:
     date = date - timedelta(days = 1)
 
 
-# In[134]:
+# In[244]:
 
 
 data_files = glob.glob(f"{data_dir}/*.csv")
 
 daily_dataframes = []
+p = re.compile(r'(\d{2}-\d{2}-\d{2})')
 
 for file in data_files:
-    daily_dataframe = pd.read_csv(file)
+    daily_dataframe = pd.read_csv(file, parse_dates=[2])
+    daily_dataframe["Last Update"] = p.search(file).group()
     daily_dataframes.append(daily_dataframe)
 
 combined_data = pd.concat(daily_dataframes, axis=0, ignore_index=True)
 combined_data = combined_data.sort_values(by="Last Update", ascending=False)
+combined_data = combined_data.drop(["Longitude", "Latitude"], axis=1)
+combined_data["Last Update"] = combined_data["Last Update"].apply(lambda x: datetime.strptime(x, "%m-%d-%y"))
+combined_data["Country/Region"] = combined_data["Country/Region"].replace("United Kingdom", "UK")
+combined_data["Country/Region"] = combined_data["Country/Region"].replace("Mainland China", "China")
 
 
-# In[178]:
+# In[247]:
 
 
-grouped_data = combined_data.groupby(by=["Last Update", "Country/Region"]).sum()
-print(grouped_data.head())
+countries_selected = ["UK"]
+filtered_data = combined_data[combined_data["Country/Region"].isin(countries_selected)]
+grouped_data = filtered_data.groupby(by=["Last Update", "Country/Region"]).sum()
+print(grouped_data.tail(10))
+fig, ax = plt.subplots(figsize=(10,7))
+clean_data = grouped_data.unstack()
+clean_data.plot(kind="line", ax=ax)
 
